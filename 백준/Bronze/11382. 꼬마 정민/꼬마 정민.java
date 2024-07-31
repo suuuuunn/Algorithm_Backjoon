27cc067747a217e2cecf189d3b4c8ac0f63c6146
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-07-31 17:58:32 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		long a, b, c;
+		a = scanner.nextLong();
+		b = scanner.nextLong();
+		c = scanner.nextLong();
+		System.out.println(a + b + c);
+		
+	}
+	
+}
