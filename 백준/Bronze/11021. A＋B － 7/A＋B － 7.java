9057ea119ebea9ee5786b3ace8ef4a95e6c82ee7
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-07-31 18:08:05 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18724 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		
+		for (int i=1; i<=n; i++) {
+			int a, b;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println("Case #" + i + ": " + (a + b));
+		}
+		
+	}
+	
+}
