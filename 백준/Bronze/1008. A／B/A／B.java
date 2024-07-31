4b8a5f4ef451e7bfa610844ddf03174edde04f04
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-07-31 17:55:51 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println((double) a / b);
+	}
+	
+}
