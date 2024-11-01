7ada7ec555bfe61ff30ef800161787df3fccceb7
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-11-01 20:21:44 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 180 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+		System.out.println(n);
+		System.out.println("1");
+	}
+	
+}
