03c7763e4e3239d2510832f5b015ef7d87ac6409
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-11-21 15:12:05 +0900

    [Silver V] Title: 영화감독 숌, Time: 216 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int n_count = 0;
+		int min = 666; int num = 666;
+		while (true) {
+			int min2 = min; int count = 0;
+			while (min2 > 0) {
+				if (min2 % 10 == 6) {
+					count++;
+				} else if (min2 % 10 != 6 && count < 3) {
+					count = 0;
+				}
+				min2 /= 10;
+			}
+			if (count >= 3) {
+				n_count++;
+			}
+			if (n_count == n) {
+				num = min;
+				break;
+			}
+			min++;
+		}
+		System.out.println(num);
+		
+	}
+	
+}
