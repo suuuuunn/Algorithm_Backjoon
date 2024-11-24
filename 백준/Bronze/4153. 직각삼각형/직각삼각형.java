e88acde4e4eeea54b5bd9bd8c70101db84cf932d
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-11-24 17:34:15 +0900

    [Bronze III] Title: 직각삼각형, Time: 168 ms, Memory: 17172 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -7,19 +7,15 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		
 		while (true) {
-			int arr[] = new int[3];
-			arr[0] = s.nextInt(); arr[1] = s.nextInt(); arr[2] = s.nextInt();
-			if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+			int a = s.nextInt(); int b = s.nextInt(); int c = s.nextInt();
+			if (a == 0 && b == 0 && c == 0) {
 				break;
 			}
-			int max = 0; int sum = 0;
-			for (int i=0; i<3; i++) {
-				sum += arr[i] * arr[i];
-				if (arr[i] > max) {
-					max = arr[i];
-				}
-			}
-			if (max * max == sum - max * max) {
+			if (a*a + b*b == c*c) {
+				System.out.println("right");
+			} else if (b*b + c*c == a*a) {
+				System.out.println("right");
+			} else if (c*c + a*a == b*b) {
 				System.out.println("right");
 			} else {
 				System.out.println("wrong");
