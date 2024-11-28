305f8204fcf0db7a415189595cd78a2b2fe422f8
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-11-28 20:33:20 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,27 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException  {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
-		int n = Integer.parseInt(br.readLine());
-		String s = n + "";
-		int arr[] = new int[s.length()];
+		int[] counting = new int[10];
+		String s = br.readLine();
+		
 		for (int i=0; i<s.length(); i++) {
-			arr[i] = s.charAt(i) - '0';
-		}
-		Arrays.sort(arr);
-		for (int i=arr.length-1; i>=0; i--) {
-			sb.append(arr[i]);
+			counting[s.charAt(i) - '0']++;
 		}
 		
-		System.out.println(sb.toString());
+		for (int i=9; i>=0; i--) {
+			while (counting[i]-- > 0) {
+				System.out.print(i);
+			}
+		}
+	
 	}
 	
 }
