985d429d22d5658eda2f2d0d6bc67999147edef3
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-12-05 17:57:39 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
 		
 		int arr[][] = new int[n][2];
@@ -21,20 +19,20 @@ public class Main {
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		int count[] = new int[n];
 		for (int i=0; i<n; i++) {
+			int rank = 1;
 			for (int j=0; j<n; j++) {
+				if (i == j) {
+					continue;
+				}
 				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
-					count[i]++;
+					rank++;
 				}
 			}
+			sb.append(rank).append(" ");
 		}
 		
-		for (int i=0; i<n; i++) {
-			bw.write((count[i] + 1) + " ");
-		}
-		bw.flush();
-		
+		System.out.println(sb);
 	}
 	
 }
