a7b83c7c639bc09b455dfb2918e77ef21064ecbf
sumi <160824008+suuuuunn@users.noreply.github.com>
2024-12-14 15:52:51 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 18648 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,45 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
+	static StringBuilder sb = new StringBuilder();
+	static int[] stack = new int[10000];
+	
+	static int size = 0;
+	static int back = 0;
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
-		
-		Stack<Integer> stack = new Stack<>();
 		
 		while (n-- > 0) {
 			st = new StringTokenizer(br.readLine(), " ");
 			
 			switch (st.nextToken()) {
-			case "push": 
-				stack.add(Integer.parseInt(st.nextToken()));
-				break;
-			case "pop":
-				if (stack.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(stack.pop()).append("\n");
-				break;
-			case "size":
-				sb.append(stack.size()).append("\n");
-				break;
-			case "empty":
-				if (stack.isEmpty()) sb.append(1).append("\n");
-				else sb.append(0).append("\n");
-				break;
-			case "top":
-				if (stack.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(stack.peek()).append("\n");
-				break;
+			case "push": push(Integer.parseInt(st.nextToken())); break;
+			case "pop": pop(); break;
+			case "size": size(); break;
+			case "empty": empty(); break;
+			case "top": top(); break;
 			}
 		}
 		System.out.println(sb);
+	}
+	
+	static void push(Integer x) {
+		stack[back] = x;
+		size++; back++;
+	}
+	
+	static void pop() {
+		if (size == 0) {
+			sb.append(-1).append("\n");
+		} else {
+			sb.append(stack[back-1]).append("\n");
+			back--; size--;
+		}
+	}
+	
+	static void size() {
+		sb.append(size).append("\n");
+	}
+	
+	static void empty() {
+		if (size == 0) sb.append(1).append("\n");
+		else sb.append(0).append("\n");
+	}
+	
+	static void top() {
+		if (size == 0) sb.append(-1).append("\n");
+		else sb.append(stack[back-1]).append("\n");
 	}
 	
 }
