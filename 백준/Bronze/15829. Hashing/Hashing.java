5f404a885e3f2cbdd0a3716afef30d7ed73cd164
sumi <160824008+suuuuunn@users.noreply.github.com>
2025-01-07 16:58:24 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14356 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -10,13 +10,13 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 		String str = br.readLine();
 		StringBuilder sb = new StringBuilder();
-		int sum = 0;
+		long sum = 0; long r = 1; int p = 1234567891;
 		for (int i=0; i<n; i++) {
-			int a = str.charAt(i) - 'a' + 1;
-			sum += a * Math.pow(31, i);
+			sum += ((str.charAt(i)-'a' + 1)*r)%p;
+			r = (31*r)%p;
 		}
-		System.out.println(sum % 1234567891);
 		
+		System.out.println(sum%p);
 	}
 
 }
