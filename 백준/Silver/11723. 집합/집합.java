d4233e775ca94b198f11140a28d4d3391b84bc4e
sumi <160824008+suuuuunn@users.noreply.github.com>
2025-01-24 15:14:33 +0900

    [Silver V] Title: 집합, Time: 1024 ms, Memory: 312756 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -4,10 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int s[] = new int[22];
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
